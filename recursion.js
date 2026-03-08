f78c6bc17ec95bc32fdc8c5ac9a04dd69ab516e7
ghostinchip <ghostinchip@gmail.com>
2026-03-08 18:13:49 +0300

    fix: recursion is done

--- recursion.js
+++ recursion.js
@@ -14,7 +14,17 @@ const e = arr => {
   return 1 + e(arr.slice(1));
 }
 
+const k = arr => {
+  if (arr.length === 1) {
+    return arr[0];
+  }
+  const subMax = k(arr.slice(1));
+  return arr[0] > subMax ? arr[0] : subMax;
+
+}
+
 const data = [1, 2, 3, 4, 5];
 
 console.log(f(data));
 console.log(e(data));
+console.log(k(data));
--- recursion.py
+++ recursion.py
@@ -1,14 +1,18 @@
 def f(arr):
-    if not arr == 0:
+    if not arr:
         return 0
     return arr[0] + f(arr[1:])
 
 def e(arr):
-    if not arr == 0:
+    if not arr:
         return 0
     return 1 + e(arr[1:])    
 
-
+def k(arr):
+    if len(arr) == 1:
+        return arr[0]
+    sub_max = k(arr[1:])
+    return arr[0] if arr[0] > sub_max else sub_max
 
 
 data = [1, 3, 4, 5]
@@ -18,3 +22,4 @@ print(e(data))
 
 print(f(data))
 print(sum(data))
+print(k(data))
